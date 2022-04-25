456ccb1823ec402e269a3a96f47807433b62d45e
Matheus Campos <matheus_501@hotmail.com>
2022-04-25 10:09:25 -0300

    adicionando for.js index.html e script.js

---
+++ for.js
@@ -0,0 +1,59 @@
+// var numeros = [1, 3, 7, 9, 12, 20, 30]
+// for (let index = 0; index < numeros.length; index++) {
+//   const element = numeros[index]
+//   console.log(element)
+// }
+
+// var numeros = [1, 3, 7, 12, 20, 30]
+// for (let i = numeros.length - 1; i >= 0; i--) {
+//   const element = numeros[i]
+//   console.log(element)
+// }
+
+// var idade = 18
+// while (idade >= 18) {
+//   console.log('pode tirar habilitação')
+//   idade = 17
+// }
+
+// var idade = 0
+// while (idade < 3) {
+//   // console.log('pode tirar habilitação')
+//   idade++
+// }
+
+// var nomes = ['maria', 'Lucia', 'manuel', 'larissa', 'matheus']
+
+// for (let i = 0; i < nomes.length; i++) {
+//   // const nome = nomes[i]
+// }
+
+// for (let i = nomes.length - 1; i >= 0; i--) {
+//   const nome = nomes[i]
+//   console.log(nome)
+// }
+// var idade = 18
+// if (idade >= 18) {
+//   console.log('ja pode beber')
+//   idade = 17
+// }
+
+// var idade = 18
+// if (idade >= 18) {
+//   console.log('pode tirar habilitação')
+// } else {
+//   console.log('não pode tirar habilitação')
+// }
+
+var nomes = ['igor', 'matheus', 'joão', 'Lucas', 'maria']
+
+// console.log(nomes)
+// for (let i = 0; i < nomes.length; i++) {
+//   const nome = nomes[i]
+//   console.log(nome)
+// }
+
+for (let i = nomes.length - 1; i >= 0; i--) {
+  const nome = nomes[i]
+  console.log(nome)
+}
--- script.js
+++ script.js
@@ -20,11 +20,41 @@ function calcularOResultadoDoTroco(buy, pay) {
   const buyNumber = Number(buy)
   const payNumber = Number(pay)
   const troco = buyNumber - payNumber
+  if (troco < 0) {
+    return alert('valor insuficiente')
+  }
   insereValorNoTextArea(troco)
 }
 
 function insereValorNoTextArea(troco) {
   const textArea = document.getElementById('result')
   textArea.innerText = troco
-  const notas = [2, 5, 10, 20, 50, 100]
+  const notas = [100, 50, 20, 10, 5, 2]
+  // o comprimento do array é 6 pois tem 6 elementos
+  //  for é uma estrutura de repetição onde eu crio uma variavel i podendo começar no 0, e depois faço uma condição para ela baseada no meu array. Portanto, se o i for menor que o tamanho do array, é pra ele adicionar i++.
+  var resto = troco
+  var qtdCedula = 0
+  for (var i = 0; i < notas.length; i++) {
+    const nota = notas[i]
+
+    while (resto >= nota) {
+      resto = resto - nota
+      qtdCedula++
+      console.log(qtdCedula)
+      console.log(nota)
+      if (resto < nota) {
+        qtdCedula = 0
+      }
+    }
+  }
 }
+
+//quando o resto for menor do que a nota subtraia ele pela nota seguinte
+
+//
+//
+
+// 1º passo: girar todas as posiçoes no array (x)
+// 2º passo: se o troco for maior ou igual a posição do array diminui ele pela posição do array.
+// 2.1 passo: criar uma variável para identificar as cedulas de troco
+// 3º passo: guardar o resultado da subtração para usar na próxima execução. ( variavel )
